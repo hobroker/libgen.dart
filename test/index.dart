16b0635c9527bcae0f8d1fd4e6e8377334b0fccf
hobroker <igor.leahu24@gmail.com>
2020-09-26 15:02:57 +0300

    Adjust how coverage is collected

--- test/index.dart
+++
@@ -1,5 +0,0 @@
-import 'mirror_test.dart' as mirror;
-
-void main() {
-  mirror.main();
-}
--- tool/coverage.sh
+++ tool/coverage.sh
@@ -2,35 +2,15 @@
 
 set -e
 
-pub get
-pub global activate coverage
-
-OBS_PORT=9292
-echo "-- Collecting coverage on port $OBS_PORT..."
-
-# Start tests in one VM.
 echo "-- Starting tests..."
-dart \
-  --disable-service-auth-codes \
-  --enable-vm-service=$OBS_PORT \
-  --pause-isolates-on-exit \
-  test/index.dart &
-
-# Run the coverage collector to generate the JSON coverage report.
-echo "-- Collecting coverage..."
-pub global run coverage:collect_coverage \
-  --port=$OBS_PORT \
-  --out=coverage/coverage.json \
-  --wait-paused \
-  --resume-isolates
+dart test \
+  --reporter=expanded \
+  --coverage=coverage
 
 echo "-- Generating LCOV report..."
 pub global run coverage:format_coverage \
   --lcov \
-  --in=coverage/coverage.json \
+  --in=coverage \
   --out=coverage/lcov.info \
   --packages=.packages \
   --report-on=lib
-
-echo "-- Converting LCOV report to HTML..."
-type genhtml && genhtml -o coverage coverage/lcov.info || echo "...skipping"
