54876a5307129a6a77658625fbdd4888ab06ee0d
hobroker <igor.leahu24@gmail.com>
2020-09-27 13:02:04 +0300

    Adjust LibgenMirror

--- lib/src/mirror.dart
+++ lib/src/mirror.dart
@@ -1,28 +1,47 @@
 import 'http_client.dart';
 import 'mirror_schema.dart';
+import 'mirror_schema_finder.dart';
 
 class LibgenMirror {
-  final bool canDownload;
+  final bool _canDownload;
 
   final HttpClient _http;
 
   LibgenMirror({
-    this.canDownload = false,
+    bool canDownload = false,
     HttpClient client,
-  }) : _http = client;
+  })  : _http = client,
+        _canDownload = canDownload;
 
-  LibgenMirror.fromSchema(MirrorSchema schema)
-      : canDownload = schema.canDownload,
-        _http = HttpClient(
+  bool get canDownload => _canDownload;
+
+  factory LibgenMirror.fromSchema(MirrorSchema schema) => LibgenMirror(
+        canDownload: schema.canDownload,
+        client: HttpClient(
           host: schema.host,
           scheme: schema.scheme,
-        );
+        ),
+      );
+
+  /// Returns a [LibgenMirror] instance
+  /// [_http] with the first [MirrorSchema] which has the shortest response
+  static Future<LibgenMirror> fastest() async {
+    final schema = await MirrorSchemaFinder().fastest();
+    return LibgenMirror.fromSchema(schema);
+  }
+
+  /// Returns a [LibgenMirror] instance
+  /// [_http]  with the first [MirrorSchema] which has a successful response
+  static Future<LibgenMirror> any() async {
+    final schema = await MirrorSchemaFinder().any();
+    return LibgenMirror.fromSchema(schema);
+  }
 
   /// Retuns a [List] of [Map] with [fields] by [ids]
-  Future<List> getByIds(List<int> ids, [String fields]) =>
+  Future<List> getByIds(List<int> ids, [String fields = '*']) =>
       _http.request('json.php', query: {
         'ids': ids.join(','),
-        'fields': fields ?? '*',
+        'fields': fields,
       });
 
   /// Returns `"pong"` if the mirror returned any data
