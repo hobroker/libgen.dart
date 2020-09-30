e28d66eac160ceeab2025c2d6c0efa1b16d8d3bb
hobroker <igor.leahu24@gmail.com>
2020-10-01 01:17:44 +0300

    Adjust example

--- example/main.dart
+++ example/main.dart
@@ -3,6 +3,7 @@ import 'package:libgen/libgen.dart';
 void main() async {
   final libgen = await Libgen.any();
 
-  final result = await libgen.getById('1591104');
-  print(result);
+  // print(await libgen.getById('1591104'));
+  print(await libgen.getById('1'));
+  print(await libgen.getByIds(['1', '2']));
 }
