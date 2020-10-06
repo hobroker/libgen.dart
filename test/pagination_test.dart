0578c633fb78aa97be5a3b0c72df40c9025c013e
hobroker <igor.leahu24@gmail.com>
2020-10-07 02:02:38 +0300

    Update pagination_test.dart

--- test/pagination_test.dart
+++ test/pagination_test.dart
@@ -5,93 +5,93 @@ void main() {
   group('Pagination', () {
     group('single page', () {
       test('full without offset', () {
-        final pagination = Pagination(25);
+        final nav = Pagination(25);
 
-        expect(pagination.limit, equals(25));
-        expect(pagination.page, equals(1));
-        expect(pagination.ignoreLast, isZero);
-        expect(pagination.ignoreFirst, isZero);
-        expect(pagination.hasNext, isFalse);
+        expect(nav.limit, 25);
+        expect(nav.page, 1);
+        expect(nav.ignoreLast, isZero);
+        expect(nav.ignoreFirst, isZero);
+        expect(nav.hasNext, isFalse);
       });
 
       test('full second page', () {
-        final pagination = Pagination(25, offset: 25);
+        final nav = Pagination(25, offset: 25);
 
-        expect(pagination.limit, equals(25));
-        expect(pagination.page, equals(2));
-        expect(pagination.ignoreLast, isZero);
-        expect(pagination.ignoreFirst, isZero);
-        expect(pagination.hasNext, isFalse);
+        expect(nav.limit, 25);
+        expect(nav.page, 2);
+        expect(nav.ignoreLast, isZero);
+        expect(nav.ignoreFirst, isZero);
+        expect(nav.hasNext, isFalse);
       });
 
       test('full fifth page', () {
-        final pagination = Pagination(5, offset: 500);
+        final nav = Pagination(5, offset: 500);
 
-        expect(pagination.limit, equals(25));
-        expect(pagination.page, equals(6));
-        expect(pagination.ignoreLast, 20);
-        expect(pagination.ignoreFirst, isZero);
-        expect(pagination.hasNext, isFalse);
+        expect(nav.limit, 25);
+        expect(nav.page, 6);
+        expect(nav.ignoreLast, 20);
+        expect(nav.ignoreFirst, isZero);
+        expect(nav.hasNext, isFalse);
       });
 
       test('partial page without offset', () {
-        final pagination = Pagination(10);
+        final nav = Pagination(10);
 
-        expect(pagination.limit, equals(25));
-        expect(pagination.page, equals(1));
-        expect(pagination.ignoreLast, equals(15));
-        expect(pagination.ignoreFirst, isZero);
-        expect(pagination.hasNext, isFalse);
+        expect(nav.limit, 25);
+        expect(nav.page, 1);
+        expect(nav.ignoreLast, 15);
+        expect(nav.ignoreFirst, isZero);
+        expect(nav.hasNext, isFalse);
       });
 
       test('partial page with offset', () {
-        final pagination = Pagination(30, offset: 5);
+        final nav = Pagination(30, offset: 5);
 
-        expect(pagination.limit, equals(50));
-        expect(pagination.page, equals(1));
-        expect(pagination.ignoreLast, equals(15));
-        expect(pagination.ignoreFirst, equals(5));
-        expect(pagination.hasNext, isFalse);
+        expect(nav.limit, 50);
+        expect(nav.page, 1);
+        expect(nav.ignoreLast, 15);
+        expect(nav.ignoreFirst, 5);
+        expect(nav.hasNext, isFalse);
       });
 
       test('partial second page with offset greater than min page size', () {
-        final pagination = Pagination(5, offset: 30);
+        final nav = Pagination(5, offset: 30);
 
-        expect(pagination.limit, equals(25));
-        expect(pagination.page, equals(2));
-        expect(pagination.ignoreLast, equals(15));
-        expect(pagination.ignoreFirst, equals(5));
-        expect(pagination.hasNext, isFalse);
+        expect(nav.limit, 25);
+        expect(nav.page, 2);
+        expect(nav.ignoreLast, 15);
+        expect(nav.ignoreFirst, 5);
+        expect(nav.hasNext, isFalse);
       });
 
       test('partial second page with offset greater than max page size', () {
-        final pagination = Pagination(5, offset: 100);
+        final nav = Pagination(5, offset: 100);
 
-        expect(pagination.limit, equals(25));
-        expect(pagination.page, equals(2));
-        expect(pagination.ignoreLast, equals(20));
-        expect(pagination.ignoreFirst, equals(0));
-        expect(pagination.hasNext, isFalse);
+        expect(nav.limit, 25);
+        expect(nav.page, 2);
+        expect(nav.ignoreLast, 20);
+        expect(nav.ignoreFirst, 0);
+        expect(nav.hasNext, isFalse);
       });
     });
 
     group('multiple pages', () {
       test('without offset', () {
-        final pagination = Pagination(120);
+        final nav = Pagination(120);
 
-        expect(pagination.limit, equals(100));
-        expect(pagination.page, equals(1));
-        expect(pagination.ignoreFirst, equals(0));
-        expect(pagination.ignoreLast, equals(0));
-        expect(pagination.hasNext, isTrue);
+        expect(nav.limit, 100);
+        expect(nav.page, 1);
+        expect(nav.ignoreFirst, 0);
+        expect(nav.ignoreLast, 0);
+        expect(nav.hasNext, isTrue);
 
-        pagination.next();
+        nav.next();
 
-        expect(pagination.limit, equals(25));
-        expect(pagination.page, equals(2));
-        expect(pagination.ignoreFirst, equals(0));
-        expect(pagination.ignoreLast, equals(5));
-        expect(pagination.hasNext, isFalse);
+        expect(nav.limit, 25);
+        expect(nav.page, 2);
+        expect(nav.ignoreFirst, 0);
+        expect(nav.ignoreLast, 5);
+        expect(nav.hasNext, isFalse);
       });
     });
   });
